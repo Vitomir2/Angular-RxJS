3dcd5c06e83ce525ee242d9a15471f4bbfc3866c
Vitomir Pavlov <v.pavlov@sigmax.nl>
2020-09-14 12:42:54 +0200

    vp - initial commit for the products application

---
+++ src/app/products/product-list-alt/product-list-alt.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { EMPTY } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'pm-product-list',
+  templateUrl: './product-list-alt.component.html'
+})
+export class ProductListAltComponent {
+  pageTitle = 'Products';
+  errorMessage = '';
+  selectedProductId: number;
+
+  products$ = this.productService.products$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    )
+
+  constructor(private productService: ProductService) { }
+
+  onSelected(productId: number): void {
+    console.log('Not yet implemented');
+  }
+}
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,35 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Product } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductListComponent {
+  pageTitle = 'Product List';
+  errorMessage = '';
+  categories;
+
+  products$ = this.productService.products$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );
+
+  constructor(private productService: ProductService) { }
+
+  onAdd(): void {
+    console.log('Not yet implemented');
+  }
+
+  onSelected(categoryId: string): void {
+    console.log('Not yet implemented');
+  }
+}
